4d0f4e45074b8ed6ed8bc7294f8d3f44926e24b1
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-08-07 14:36:57 +0200

    Group objects in scene by shader and then by mesh

--- include/renderer.h
+++ include/renderer.h
@@ -34,7 +34,7 @@ class Scene {
         std::vector<Object*> objects;
         glm::vec3 cam_pos {0.0f, 0.0f, -6.0f};
 
-        std::unordered_map<Shader*, std::vector<Object*>> objects_by_shader;
+        std::unordered_map<Shader*, std::unordered_map<Mesh*, std::vector<Object*>>> objects_by_mesh_by_shader;
 
         void add_object_to_scene(Object* object);
 
