7c0fe667547262d394269717b4ad27c52f1ba283
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-08-24 15:20:27 +0200

    Remove shader/mesh group from scene if removed object is the only one with this shader/mesh

--- src/renderer/scene.cpp
+++ src/renderer/scene.cpp
@@ -9,10 +9,23 @@ void Scene::add_object_to_scene(Object* object) {
 void Scene::remove_object_from_scene(Object* object) {
     auto& objects_for_mesh = objects_by_mesh_by_shader[object->material->shader][object->mesh]; // find object mesh group
 
-    auto it = std::find(objects_for_mesh.begin(), objects_for_mesh.end(), object); // Find object in group
-    if (it != objects_for_mesh.end()) {
-        objects_for_mesh.erase(it); // Delete it
+    // if it is the only object with this shader, remove the shader group from the scene
+    if (objects_by_mesh_by_shader[object->material->shader].size() == 1) {
+        objects_by_mesh_by_shader.erase(object->material->shader);
     }
+
+    // else if it is the only object with this mesh, remove the mesh group from the scene
+    else if (objects_for_mesh.size() == 1) {
+        objects_by_mesh_by_shader[object->material->shader].erase(object->mesh);
+    }
+    else {
+        // else just remove from the group
+        auto it = std::find(objects_for_mesh.begin(), objects_for_mesh.end(), object); // Find object in group
+        if (it != objects_for_mesh.end()) {
+            objects_for_mesh.erase(it); // Delete it
+        }
+    }
+
 
     auto it2 = std::find(objects.begin(), objects.end(), object); // Remove from main object list
     if (it2 != objects.end()) {
